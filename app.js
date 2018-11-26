f073b0e502bb62cce9f4b39c1b8223b2f96c9162
Tatsiana Shulha <taniaissohappy@gmail.com>
2018-11-26 09:35:47 -0600

    config api key for the api call

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 searchButton.addEventListener('click', searchWeather);
 function searchWeather () {
-    console.log(searchCity.value);
+    var cityName = searchCity.value;
+    var http = new XMLHttpRequest();
+    var apiKey = config.API_KEY;
+    var url = `http://api.openweathermap.org/data/2.5/weather?units=imperial&q=${cityName}&APPID=${apiKey}`;
 }
