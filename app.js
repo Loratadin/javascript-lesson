0e79c9dc08a0712f3e514efcd00102871cd8744a
Tatsiana Shulha <taniaissohappy@gmail.com>
2018-11-26 14:53:07 -0600

    add an alert message

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 searchButton.addEventListener('click', searchWeather);
 function searchWeather () {
     var cityName = searchCity.value;
+    if (cityName.trim().length == 0) {
+        return alert("Please, enter a city name");
+    }
     var http = new XMLHttpRequest();
     var apiKey = config.API_KEY;
-    var url = `http://api.openweathermap.org/data/2.5/weather?units=imperial&q=${cityName}&APPID=${apiKey}`;
+    var url = 'http://api.openweathermap.org/data/2.5/weather?units=imperial&q=' + cityName
 }
