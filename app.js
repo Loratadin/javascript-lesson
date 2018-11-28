de02cc6cb498779fbc37573e723c150efde1cfbf
Tatsiana Shulha <taniaissohappy@gmail.com>
2018-11-28 11:51:19 -0600

    make an API call

--- app.js
+++ app.js
@@ -5,17 +5,20 @@ function searchWeather () {
         return alert("Please, enter a city name");
     }
     var http = new XMLHttpRequest();
-    var apiKey = config.API_KEY;
-    var url = 'http://api.openweathermap.org/data/2.5/weather?units=imperial&q=' + cityName
+    var apiKey = API_KEY;
+    var url = 'http://api.openweathermap.org/data/2.5/weather?units=imperial&q=' + cityName + '&units=metric&appid=' + apiKey;
     var method = 'GET';
 
     http.open(method, url);
     http.onreadystatechange = function() {
         if(http.readyState == XMLHttpRequest.DONE && http.status === 200) {
             var data = JSON.parse(http.responseText);
-            var weatherData = new Weather(cityName, data.weather[0].description.toUpperCase());
+            console.log(data);
+            // var weatherData = new Weather(cityName, data.weather[0].description.toUpperCase());
+            // weatherData.temperature = data.main.temp;
         } else if(http.readyState === XMLHttpRequest.DONE) {
             alert("Something went wrong!");
         }
-    }
+    };
+    http.send();
 }
